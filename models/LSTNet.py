68c50b124fe72560b36ad8a87ea21eb144cb53a3
gaieepo <gaieepo@gmail.com>
2018-08-13 18:39:27 +0800

    fix integer division for python3

--- models/LSTNet.py
+++ models/LSTNet.py
@@ -13,7 +13,7 @@ class Model(nn.Module):
         self.hidS = args.hidSkip;
         self.Ck = args.CNN_kernel;
         self.skip = args.skip;
-        self.pt = (self.P - self.Ck)/self.skip
+        self.pt = (self.P - self.Ck)//self.skip
         self.hw = args.highway_window
         self.conv1 = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m));
         self.GRU1 = nn.GRU(self.hidC, self.hidR);
@@ -30,24 +30,24 @@ class Model(nn.Module):
             self.output = F.sigmoid;
         if (args.output_fun == 'tanh'):
             self.output = F.tanh;
- 
+
     def forward(self, x):
         batch_size = x.size(0);
-        
+
         #CNN
         c = x.view(-1, 1, self.P, self.m);
         c = F.relu(self.conv1(c));
         c = self.dropout(c);
         c = torch.squeeze(c, 3);
-        
-        # RNN 
+
+        # RNN
         r = c.permute(2, 0, 1).contiguous();
         _, r = self.GRU1(r);
         r = self.dropout(torch.squeeze(r,0));
 
-        
+
         #skip-rnn
-        
+
         if (self.skip > 0):
             s = c[:,:, int(-self.pt * self.skip):].contiguous();
             s = s.view(batch_size, self.hidC, self.pt, self.skip);
@@ -57,9 +57,9 @@ class Model(nn.Module):
             s = s.view(batch_size, self.skip * self.hidS);
             s = self.dropout(s);
             r = torch.cat((r,s),1);
-        
+
         res = self.linear1(r);
-        
+
         #highway
         if (self.hw > 0):
             z = x[:, -self.hw:, :];
@@ -67,11 +67,7 @@ class Model(nn.Module):
             z = self.highway(z);
             z = z.view(-1,self.m);
             res = res + z;
-            
+
         if (self.output):
             res = self.output(res);
         return res;
-    
-        
-        
-        
